326a7d49843bf7b2da6bda0f3e501d9cfbade97d
Zacharie Montreuil <zmontreuil@rrc.ca>
2025-02-06 08:49:19 -0600

    Add some basic event listeners

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-const newHeaderNode = document.createElement("h1");
-newHeaderNode.textContent = "Js-injected Header";
-newHeaderNode.classList.add("red-text");
-document.body.appendChild(newHeaderNode);
+// step 1: select elements
+const submitNameNode = document.querySelector("#submit-name");
+const nameFieldNode = document.querySelector("#field-name");
+const changeNameParagraph = document.querySelector("p");
 
-const newParagraphNode = document.createElement("p");
-newParagraphNode.textContent = "JS-injected Paragraph";
-newParagraphNode.id = "first-paragraph";
-document.body.appendChild(newParagraphNode);
+// step 2: respond to button click
+// using an anonymous function ( () => {} )
+submitNameNode.addEventListener("click", () => {
+    changeNameParagraph.textContent = `Welcome to the page, ${nameFieldNode.value}`;
+    nameFieldNode.value = "";
+});
 
-const newSpanNode = document.createElement("span");
-newSpanNode.textContent = " and Js-injected span";
-newSpanNode.classList.add("red-text");
-newParagraphNode.insertAdjacentElement("afterbegin", newSpanNode);
+function logEvent() {
+    console.log("Event occurred");
+}
 
-// newParagraphNode.removeChild(newSpanNode);
-// newParagraphNode.parentElement.removeChild(newParagraphNode);
-newParagraphNode.remove();
+// event listeners are special functions that tell DOM nodes to wait for something to happen
+// when that does happen, they invoke whatever function was provided to THEM
+// first argument: event type (string) DOMContentLoaded: page was loaded
+// second argument: function to invoke when that event occurs (callback function)
+// document.addEventListener("click", logEvent);
